149d4217b39acb81f6f357435edaede1e556f7f3
Sergie <sergiearruda@gmail.com>
2021-01-10 12:47:15 +0000

    fixed classNames to better suit 7-1 SCSS Architecture

--- src/components/pages/More.jsx
+++ src/components/pages/More.jsx
@@ -3,38 +3,38 @@ import pageIdeasLogo from "../../assets/Page ideas logo.svg";
 
 const More = () => {
   return (
-    <section id="discoverSection">
-      <div className="discover-box">
+    <section className="more">
+      <div className="more__box">
         <a
-          className="discover-buttons"
+          className="more__buttons"
           href="https://github.com/Sergie-AGA"
           target="_blank">
-          <div className="discover-buttons__container">
+          <div className="more__buttons__container">
             <i className="fab fa-github" />
-            <p className="discover-buttons__label">GitHub</p>
+            <p className="more__buttons__label">GitHub</p>
           </div>
         </a>
 
         <a
-          className="discover-buttons"
+          className="more__buttons"
           href="https://sergie-aga.github.io/Page_ideas/"
           target="_blank">
-          <div className="discover-buttons__container">
+          <div className="more__buttons__container">
             <img
               src={pageIdeasLogo}
               alt="Page Ideas logo"
-              className="discover-buttons__icon"
+              className="more__buttons__icon"
             />
-            <p className="discover-buttons__label">Page Ideas</p>
+            <p className="more__buttons__label">Page Ideas</p>
           </div>
         </a>
         <a
-          className="discover-buttons"
+          className="more__buttons"
           href="https://api-sea-by-sieger.herokuapp.com/"
           target="_blank">
-          <div className="discover-buttons__container">
+          <div className="more__buttons__container">
             <i className="fas fa-ship" />
-            <p className="discover-buttons__label">API Sea</p>
+            <p className="more__buttons__label">API Sea</p>
           </div>
         </a>
       </div>
