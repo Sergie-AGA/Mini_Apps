047ca0bd45c8159c7a510fb3e910053b080d7022
Sergie <sergiearruda@gmail.com>
2021-01-11 13:26:02 +0000

    Finished rendering elements on the home page

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Project = ({ project: { title, icon } }) => {
+const Project = ({ project: { title, icon, description, react, link } }) => {
   return (
     <div className="project">
-      <img src={icon} alt="" className="round-img" style={{ width: "60px" }} />
-      <h3> {title} </h3>
       <div>
-        {/* <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+        <img
+          src={icon}
+          alt=""
+          className="project__img"
+          style={{ width: "60px" }}
+        />
+        {react ? (
+          "Pending implementation"
+        ) : (
+          <a href={link} className="project__button" target="_blank">
+            Access
+          </a>
+        )}
+      </div>
+      <div>
+        <h3 className="project__title"> {title} </h3>
+        <p className="project__description"> {description} </p>
+      </div>
+
+      {/* <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
         </Link> */}
-      </div>
     </div>
   );
 };
