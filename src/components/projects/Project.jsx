7a224169313f516b9dbf9a86fe4302b3fae9bb5a
Sergie <sergiearruda@gmail.com>
2021-01-11 12:27:33 +0000

    Begins adding state and rendering projects

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Project = () => {
+const Project = ({ project: { title, icon } }) => {
   return (
-    <div className="card text-center">
-      <img
-        src={avatar_url}
-        alt=""
-        className="round-img"
-        style={{ width: "60px" }}
-      />
-      <h3> {login} </h3>
+    <div className="project">
+      <img src={icon} alt="" className="round-img" style={{ width: "60px" }} />
+      <h3> {title} </h3>
       <div>
-        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+        {/* <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
