13e031f49330b020480b2d3bee3119bbf04a8489
Sergie <sergiearruda@gmail.com>
2021-01-13 11:26:19 +0000

    Made Navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -7,10 +7,27 @@ import About from "./components/pages/About";
 import More from "./components/pages/More";
 
 function App() {
+  const [burgerMenu, setBurger] = useState("burger-menu__inside");
+  const [activeMenu, setMenu] = useState("navbar__items");
+
+  function toggleMenu(burgerMenu) {
+    if (burgerMenu == "burger-menu__inside") {
+      setBurger("burger-menu__inside open");
+      setMenu("navbar__items open-mobile");
+    } else {
+      setBurger("burger-menu__inside");
+      setMenu("navbar__items");
+    }
+  }
+
   return (
     <Fragment>
       <Router>
-        <Navbar />
+        <Navbar
+          toggleMenu={toggleMenu}
+          burgerMenu={burgerMenu}
+          activeMenu={activeMenu}
+        />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Navbar = ({ icon, title }) => {
+const Navbar = ({ icon, title, toggleMenu, burgerMenu, activeMenu }) => {
+  function handleClick() {
+    toggleMenu(burgerMenu);
+  }
+
   return (
-    <nav className="navbar">
-      <div className="navbar__main">
-        <i className={icon} />
-        <h1 className="navbar__title">{title}</h1>
-      </div>
-      <ul className="navbar__items">
-        <li className="navbar__items__item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar__items__item">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="navbar__items__item">
-          <Link to="/more">More resources</Link>
-        </li>
-      </ul>
-    </nav>
+    <Fragment>
+      <nav className="navbar">
+        <div className="navbar__main">
+          <i className={icon} />
+          <h1 className="navbar__title">{title}</h1>
+        </div>
+        <div className="burger-menu" onClick={handleClick}>
+          <div className={burgerMenu}></div>
+        </div>
+        <ul className={activeMenu}>
+          <li className="navbar__items__item">
+            <Link to="/" onClick={handleClick}>
+              Home
+            </Link>
+          </li>
+          <li className="navbar__items__item">
+            <Link to="/about" onClick={handleClick}>
+              About
+            </Link>
+          </li>
+          <li className="navbar__items__item">
+            <Link to="/more" onClick={handleClick}>
+              More resources
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </Fragment>
   );
 };
 
